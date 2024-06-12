250cba5a0b955da5aa580113e5343092447786b9
IcedTea <fistfireace37@gmail.com>
2024-06-13 00:04:08 +0800

    Add Task function

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -1,13 +1,19 @@
 <?php
 
-use function Livewire\Volt\{state};
-
-//
+use function Livewire\Volt\{state, with};
 
 state(['task']);
 
+with([
+    'todos'=>fn()=>\App\Models\Todo::all()
+]);
+
 $add = function() {
-    $this->task = '';
+    \App\Models\Todo::create([
+        'user_id' => auth()->id(),
+        'task' => $this->task
+    ]);
+    // $this->task = '';
 };
 
 ?>
@@ -19,5 +25,12 @@ $add = function() {
     </form>
     <div class="mt-2">
         {{ $task }}
+    </div>
+    <div>   
+        <div>
+            @foreach ($todos as $todo)
+                {{ $todo->task  }}
+            @endforeach
+        </div>
     </div>
 </div>
