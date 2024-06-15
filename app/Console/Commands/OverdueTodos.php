84cf015e2d5528f154d4cd4a431994b1f7966ce6
IcedTea <fistfireace37@gmail.com>
2024-06-15 21:21:47 +0800

    Test function, overdue todos

---
+++ app/Console/Commands/OverdueTodos.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class OverdueTodos extends Command
+{
+    protected $signature = 'todos:overdue';
+
+      protected $description = 'Command description';
+
+    public function handle()
+    {
+        $this->line('Overdue Todos:');
+    }
+}
